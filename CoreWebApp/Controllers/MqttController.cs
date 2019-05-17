d3492d0de2f733d93e971afff79bc4c4c6bc85b1
niraj.sanghani <njpsanghani@gmail.com>
2019-05-17 18:04:44 +0530

    commit1

---
+++ CoreWebApp/Controllers/MqttController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.Rpc;
+using MQTTnet.Protocol;
+
+
+
+namespace CoreWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class MqttController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var rpcClient = new MqttRpcClient(mqttClient);
+
+    var options = new MqttClientOptionsBuilder()
+        .WithWebSocketServer("10.195.97.228:1883/mqtt")
+    .Build();
+
+        mqttClient.ConnectAsync(options);
+        
+
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("MyTopic")
+            .WithPayload("Hello World")
+            .WithExactlyOnceQoS()
+            .WithRetainFlag()
+            .Build();
+
+        var timeout = TimeSpan.FromSeconds(5);
+        var qos = MqttQualityOfServiceLevel.AtMostOnce;
+        var response = rpcClient.ExecuteAsync(timeout, "myMethod", "Hello World", qos);
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CoreWebApp/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Reflection;
+
+
+
+namespace CoreWebApp
+{
+    public class Startup
+    {
+
+        #region snippet_ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // services.AddDbContext<TodoContext>(opt => 
+            //     opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+                //.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                // c.SwaggerDoc("v1", new Info
+                // {
+                //     Version = "v1",
+                //     Title = "ToDo API",
+                //     Description = "A simple example ASP.NET Core Web API",
+                //     TermsOfService = "None",
+                //     Contact = new Contact
+                //     {
+                //         Name = "Shayne Boyer",
+                //         Email = string.Empty,
+                //         Url = "https://twitter.com/spboyer"
+                //     },
+                //     License = new License
+                //     {
+                //         Name = "Use under LICX",
+                //         Url = "https://example.com/license"
+                //     }
+                // });
+
+                // // Set the comments path for the Swagger JSON and UI.
+                // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                // c.IncludeXmlComments(xmlPath);
+
+                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+        
+            //services.AddSingleton<IHostedService, GracePeriodManagerService>();
+        }
+        #endregion
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // public void ConfigureServices(IServiceCollection services)
+        // {
+        //     services.AddMvc();
+        // }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseStaticFiles();
+
+            app.UseSwagger();
+
+             app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
